c6e4e7b0aa4cd02c07a1701c0b3fe6e3280fda38
Anton Nikulin <dr.tallin@gmail.com>
2014-02-16 19:25:49 +0200

    + doc string with task

--- 2/2-2/script.py
+++ 2/2-2/script.py
@@ -1,9 +1,17 @@
 from pymongo import MongoClient
 
-#connect on default host and port
+"""
+document example:
+{ "_id" : { "$oid" : "50906d7fa3c412bb040eb577" }, "student_id" : 0, "type" : "exam", "score" : 54.6535436362647 }
+Write a program in the language of your choice that will remove the grade of type "homework"
+with the lowest score for each student from the dataset that you imported in HW 2.1.
+Since each document is one grade, it should remove one document per student.
+"""
+#connect on default host and port at localhost:27017
 client = MongoClient()
 
 db = client['students']
 collection = db['grades']
 
-print collection.find_one()
+for rec in collection.find({'type':'homework'}).sort([('student_id',1),('score',1)]):
+    print rec['student_id']
